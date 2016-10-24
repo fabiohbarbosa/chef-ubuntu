a678df54d26488e1fad146c6ef6fe39f5d897cbd
fabiohbarbosa <fabiohbarbosa@gmail.com>
2016-10-24 10:34:56 -0200

    Git config

---
+++ cookbooks/my-git/recipes/default.rb
@@ -0,0 +1,10 @@
+bash 'git config' do
+  code <<-EOH
+    if [ $SUDO_USER ]; then USER=$SUDO_USER; else USER=`whoami`; fi
+
+    su $USER -c "git config --global credential.helper cache"
+    su $USER -c "git config --global user.email "fabiohbarbosa@gmail.com""
+    su $USER -c "git config --global user.name "Fabio H G Barbosa""
+
+    EOH
+end
