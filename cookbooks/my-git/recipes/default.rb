e5e925f8c7a75f7ca44f3340951599f20436226c
fabiohbarbosa <fabiohbarbosa@gmail.com>
2016-10-24 14:37:30 -0200

    Changing cache to store

--- cookbooks/my-git/recipes/default.rb
+++ cookbooks/my-git/recipes/default.rb
@@ -2,7 +2,7 @@ bash 'git config' do
   code <<-EOH
     if [ $SUDO_USER ]; then USER=$SUDO_USER; else USER=`whoami`; fi
 
-    su $USER -c "git config --global credential.helper cache"
+    su $USER -c "git config --global credential.helper store"
     su $USER -c "git config --global user.email "fabiohbarbosa@gmail.com""
     su $USER -c "git config --global user.name "Fabio H G Barbosa""
 
