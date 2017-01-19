372c7508fef110a664a551be8486e49d05210ce5
Fabio Henrique Gonzales Barbosa <fbarbosa@atech.com.br>
2017-01-19 10:58:49 -0200

    Adding cookbooks

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo apt-get update
 sudo rm -rf local-mode-cache/ nodes/ Berksfile.lock
 cd docker && \
   docker-compose stop && \
--- install.sh
+++ install.sh
@@ -4,13 +4,14 @@ exists()
   command -v "$1" >/dev/null 2>&1
 }
 
+sudo apt-get update
+
 if ! exists ruby; then
   sudo apt-get install ruby-full build-essential -y
 else
   sudo apt-get install build-essential -y
 fi
 
-sudo apt-get install build-essential -y
 sudo gem update --no-rdoc --no-ri
 sudo gem install berkshelf ohai chef --no-rdoc --no-ri
 berks vendor vendor
